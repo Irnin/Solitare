097dc16efdb0990fd596f6f56c6962512cec0dd2
Łukasz Michalak <lupinummail@gmail.com>
2024-08-31 06:22:57 +0200

    Initial Commit

---
+++ Solitare/SolitareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolitareApp.swift
+//  Solitare
+//
+//  Created by Łukasz Michalak on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SolitareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
